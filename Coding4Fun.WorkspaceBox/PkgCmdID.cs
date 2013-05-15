747722f192aecfb44d655b5c3d159fbf04dd2a29
Sergii Bomko <aquila.ua@gmail.com>
2013-05-15 17:33:16 +0300

    sceleton for extension

---
+++ Coding4Fun.WorkspaceBox/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace aquila.Coding4Fun_WorkspaceBox
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidTestCmd =        0x100;
+
+
+    };
+}

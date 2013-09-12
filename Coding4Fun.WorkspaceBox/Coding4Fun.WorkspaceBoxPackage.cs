8caf25183a8f700f83d2fdd4a04bd2a624bc53df
Sergii Bomko <aquila.ua@gmail.com>
2013-09-12 19:36:49 +0300

    added auto update of current workspace

--- Coding4Fun.WorkspaceBox/Coding4Fun.WorkspaceBoxPackage.cs
+++ Coding4Fun.WorkspaceBox/Coding4Fun.WorkspaceBoxPackage.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Globalization;
 using System.Runtime.InteropServices;
 using System.ComponentModel.Design;
 using EnvDTE;
@@ -11,23 +9,8 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Aquila.Coding4Fun_WorkspaceBox
 {
-	/// <summary>
-	/// This is the class that implements the package exposed by this assembly.
-	///
-	/// The minimum requirement for a class to be considered a valid package for Visual Studio
-	/// is to implement the IVsPackage interface and register itself with the shell.
-	/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-	/// to do it: it derives from the Package class that provides the implementation of the 
-	/// IVsPackage interface and uses the registration attributes defined in the framework to 
-	/// register itself and its components with the shell.
-	/// </summary>
-	// This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-	// a package.
 	[PackageRegistration(UseManagedResourcesOnly = true)]
-	// This attribute is used to register the information needed to show this package
-	// in the Help/About dialog of Visual Studio.
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-	// This attribute is needed to let the shell know that this package exposes some menus.
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	[Guid(GuidList.guidCoding4Fun_WorkspaceBoxPkgString)]
 	[ProvideAutoLoad("F1536EF8-92EC-443C-9ED7-FDADF150DA82")]
@@ -35,37 +18,21 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 	{
 		private readonly int _workspaceBoxId;
 		private readonly int _checkoutId;
-		private readonly WorkspaceInfo _workspaceInfo;
+		
+		private string _currentDirectory;
+		private WorkspaceInfo _workspaceInfo;
 
-		/// <summary>
-		/// Default constructor of the package.
-		/// Inside this method you can place any initialization code that does not require 
-		/// any Visual Studio service because at this point the package object is created but 
-		/// not sited yet inside Visual Studio environment. The place to do all the other 
-		/// initialization is the Initialize method.
-		/// </summary>
 		public Coding4Fun_WorkspaceBoxPackage()
 		{
-			Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
 			_workspaceBoxId = (int)PkgCmdIDList.cmdidWorkspaceBoxCmd;
 			_checkoutId = (int)PkgCmdIDList.cmdidCheckoutCmd;
-			_workspaceInfo = Workstation.Current.GetLocalWorkspaceInfo(Environment.CurrentDirectory);
 		}
 
-		/////////////////////////////////////////////////////////////////////////////
-		// Overridden Package Implementation
 		#region Package Members
-
-		/// <summary>
-		/// Initialization of the package; this method is called right after the package is sited, so this is the place
-		/// where you can put all the initialization code that rely on services provided by VisualStudio.
-		/// </summary>
 		protected override void Initialize()
 		{
-			Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
 			base.Initialize();
 
-			// Add our command handlers for menu (commands must exist in the .vsct file)
 			var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
 			if (null != mcs)
 			{
@@ -103,16 +70,17 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 		private void OnCheckoutQueryStatus(object sender, EventArgs e)
 		{
 			var menuCommand = sender as OleMenuCommand;
-			if (menuCommand != null)
+			if (menuCommand == null)
 			{
-				menuCommand.Supported = false;
+				return;
+			}
 
-				if (HasWorkspace())
-				{
-					menuCommand.Supported = true;
-					menuCommand.Visible = true;
-					menuCommand.Text = Resources.CheckoutCurrentFile;
-				}
+			menuCommand.Supported = false;
+			if (HasWorkspace())
+			{
+				menuCommand.Supported = true;
+				menuCommand.Visible = true;
+				menuCommand.Text = Resources.CheckoutCurrentFile;
 			}
 		}
 
@@ -123,23 +91,34 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 
 		private string GetCurrentWorkspace()
 		{
+			var dte = (_DTE)GetService(typeof(_DTE));
+			if (string.IsNullOrEmpty(_currentDirectory) ||
+				_currentDirectory != dte.Solution.FullName)
+			{
+				_currentDirectory = dte.Solution.FullName;
+				_workspaceInfo = Workstation.Current.GetLocalWorkspaceInfo(_currentDirectory);
+			}
 			return HasWorkspace() ? _workspaceInfo.DisplayName : Resources.NoWorkspace;
 		}
 
 		private void CheckoutCurFile()
 		{
 			var app = (DTE)GetService(typeof(SDTE));
-			if (app.ActiveDocument != null)
+			if (app.ActiveDocument == null)
 			{
-				var text = (TextDocument)app.ActiveDocument.Object(String.Empty);
-				string activeDocumentFullName = app.ActiveDocument.FullName;
-				if (text != null && text.Type.Equals("Text") && _workspaceInfo != null)
-				{
-					var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
-					var workspace = _workspaceInfo.GetWorkspace(server);
-					workspace.PendEdit(activeDocumentFullName);
-				}
+				return;
 			}
+
+			var text = (TextDocument)app.ActiveDocument.Object(String.Empty);
+			var activeDocumentFullName = app.ActiveDocument.FullName;
+			if (text == null || !text.Type.Equals("Text") || !HasWorkspace())
+			{
+				return;
+			}
+
+			var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
+			var workspace = _workspaceInfo.GetWorkspace(server);
+			workspace.PendEdit(activeDocumentFullName);
 		}
 	}
 }

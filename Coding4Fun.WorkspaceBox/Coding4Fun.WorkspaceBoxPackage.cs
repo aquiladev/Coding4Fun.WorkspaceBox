e43fe32393cf8b74c2a243ef5cc14f61c48559b0
Sergii Bomko <aquila.ua@gmail.com>
2013-10-25 17:16:23 +0300

    cleared vsct, added logging stuff

--- Coding4Fun.WorkspaceBox/Coding4Fun.WorkspaceBoxPackage.cs
+++ Coding4Fun.WorkspaceBox/Coding4Fun.WorkspaceBoxPackage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.ComponentModel.Design;
@@ -14,7 +15,7 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 	[PackageRegistration(UseManagedResourcesOnly = true)]
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
 	[ProvideMenuResource("Menus.ctmenu", 1)]
-	[Guid(GuidList.guidCoding4Fun_WorkspaceBoxPkgString)]
+	[Guid(GuidList.GuidCoding4Fun_WorkspaceBoxPkgString)]
 	[ProvideAutoLoad("F1536EF8-92EC-443C-9ED7-FDADF150DA82")]
 	public sealed class Coding4Fun_WorkspaceBoxPackage : Package
 	{
@@ -34,26 +35,31 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 		#region Package Members
 		protected override void Initialize()
 		{
+			Debug.WriteLine("================Initialize {0}====================", _i1++);
 			base.Initialize();
 
 			var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-			if (null != mcs)
+			if (null == mcs)
 			{
-				var cmdId = new CommandID(GuidList.guidCoding4Fun_WorkspaceBoxCmdSet, _workspaceBoxId);
-				var workspaceItem = new OleMenuCommand(OnGoToSourceControl, cmdId);
-				workspaceItem.BeforeQueryStatus += OnExec;
-				mcs.AddCommand(workspaceItem);
-
-				var checkoutBtnId = new CommandID(GuidList.guidCoding4Fun_CheckoutCmdSet, _checkoutId);
-				var checkoutItem = new OleMenuCommand(OnCheckoutExec, checkoutBtnId);
-				checkoutItem.BeforeQueryStatus += OnCheckoutQueryStatus;
-				mcs.AddCommand(checkoutItem);
+				return;
 			}
+
+			var cmdId = new CommandID(GuidList.GuidCoding4FunCmdSet, _workspaceBoxId);
+			var workspaceItem = new OleMenuCommand(OnGoToSourceControl, cmdId);
+			workspaceItem.BeforeQueryStatus += OnExec;
+			mcs.AddCommand(workspaceItem);
+
+			var checkoutBtnId = new CommandID(GuidList.GuidCoding4FunCmdSet, _checkoutId);
+			var checkoutItem = new OleMenuCommand(OnCheckoutExec, checkoutBtnId);
+			checkoutItem.BeforeQueryStatus += OnCheckoutQueryStatus;
+			mcs.AddCommand(checkoutItem);
 		}
 		#endregion
 
 		private void OnExec(object sender, EventArgs e)
 		{
+			//~300 t
+			Debug.WriteLine("================OnExec {0}====================", _oe1++);
 			var menuCommand = sender as OleMenuCommand;
 			if (menuCommand != null)
 			{
@@ -63,6 +69,7 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 
 		private void OnGoToSourceControl(object sender, EventArgs e)
 		{
+			Debug.WriteLine("================OnGoToSourceControl {0}====================", _ogs1++);
 			var menuCommand = sender as OleMenuCommand;
 			if (menuCommand != null)
 			{
@@ -87,6 +94,8 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 
 		private void OnCheckoutQueryStatus(object sender, EventArgs e)
 		{
+			//~280 t
+			Debug.WriteLine("================OnCheckoutQueryStatus {0}====================", _ocq1++);
 			var menuCommand = sender as OleMenuCommand;
 			if (menuCommand == null)
 			{
@@ -115,6 +124,7 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 
 		private void OnCheckoutExec(object sender, EventArgs e)
 		{
+			Debug.WriteLine("================OnCheckoutExec {0}====================", _oce1++);
 			var menuCommand = sender as OleMenuCommand;
 			if (menuCommand != null)
 			{
@@ -124,11 +134,15 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 
 		private bool HasWorkspace()
 		{
+			//~ 320 t
+			Debug.WriteLine("================HasWorkspace {0}====================", _hw1++);
 			return _workspaceInfo != null;
 		}
 
 		private string GetCurrentWorkspace()
 		{
+			//~ 320 t
+			Debug.WriteLine("================GetCurrentWorkspace {0}====================", _gcw1++);
 			var dte = (_DTE)GetService(typeof(_DTE));
 			if (string.IsNullOrEmpty(_currentDirectory) ||
 				_currentDirectory != dte.Solution.FullName)
@@ -141,28 +155,26 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 
 		private bool HasPandingChangesAlready()
 		{
+			// 1 t
+			Debug.WriteLine("================HasPandingChangesAlready {0}====================", _hpca++);
 			var app = (DTE)GetService(typeof(SDTE));
 			var doc = app.ActiveDocument == null
 				? null
 				: (TextDocument)app.ActiveDocument.Object();
-			if (!IsCanPendEdit(doc))
+			if (!IsCanPendEdit(doc) || app.ActiveDocument == null)
 			{
 				return false;
 			}
 
-			if (app.ActiveDocument != null)
-			{
-				var activeDocumentFullName = app.ActiveDocument.FullName;
-				var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
-				var workspace = _workspaceInfo.GetWorkspace(server);
-				return workspace.GetPendingChanges(activeDocumentFullName).Any();
-			}
-
-			return false;
+			var activeDocumentFullName = app.ActiveDocument.FullName;
+			var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
+			var workspace = _workspaceInfo.GetWorkspace(server);
+			return workspace.GetPendingChanges(activeDocumentFullName).Any();
 		}
 
 		private void CheckoutCurFile()
 		{
+			Debug.WriteLine("================CheckoutCurFile {0}====================", _ccf++);
 			var app = (DTE)GetService(typeof(SDTE));
 			var doc = app.ActiveDocument == null
 				? null
@@ -180,13 +192,28 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 
 		private static bool IsCanPendEdit(TextDocument doc)
 		{
+			// 1 t
+			Debug.WriteLine("================IsCanPendEdit {0}====================", _icpe++);
 			return doc != null && doc.Type.Equals("Text");
 		}
 
 		private static void RefreshPendingChanges(_DTE application)
 		{
+			Debug.WriteLine("================RefreshPendingChanges {0}====================", _rpc++);
 			application.ExecuteCommand("View.TfsPendingChanges");
 			application.ExecuteCommand("View.Refresh");
 		}
+
+		private int _i1;
+		private int _oe1;
+		private int _ogs1;
+		private int _ocq1;
+		private int _oce1;
+		private int _hw1;
+		private int _gcw1;
+		private int _hpca;
+		private int _ccf;
+		private static int _icpe;
+		private static int _rpc;
 	}
 }
--- Coding4Fun.WorkspaceBox/Guids.cs
+++ Coding4Fun.WorkspaceBox/Guids.cs
@@ -6,11 +6,9 @@ namespace Aquila.Coding4Fun_WorkspaceBox
 {
 	static class GuidList
 	{
-		public const string guidCoding4Fun_WorkspaceBoxPkgString = "593fa676-e509-443f-b1de-2237d5f7e545";
-		public const string guidCoding4Fun_WorkspaceBoxCmdSetString = "458389fd-2202-4e28-9113-d4cb39de5a11";
-		public const string guidCoding4Fun_CheckoutCmdSetString = "548389fd-2202-4e28-9113-d4cb39de5a22";
+		public const string GuidCoding4Fun_WorkspaceBoxPkgString = "593fa676-e509-443f-b1de-2237d5f7e545";
+		public const string GuidCoding4FunCmdSetString = "1508C2F6-4C2C-4880-B5E2-64750036AADF";
 
-		public static readonly Guid guidCoding4Fun_WorkspaceBoxCmdSet = new Guid(guidCoding4Fun_WorkspaceBoxCmdSetString);
-		public static readonly Guid guidCoding4Fun_CheckoutCmdSet = new Guid(guidCoding4Fun_CheckoutCmdSetString);
+		public static readonly Guid GuidCoding4FunCmdSet = new Guid(GuidCoding4FunCmdSetString);
 	};
 }

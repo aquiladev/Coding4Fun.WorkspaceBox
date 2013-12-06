e5ce214ca7074309aa4a8be11699c2159cb871ee
Sergii Bomko <aquila.ua@gmail.com>
2013-12-06 22:31:46 +0200

    rollback losted changes

--- Coding4Fun.WorkspaceBox/Coding4Fun.WorkspaceBoxPackage.cs
+++ Coding4Fun.WorkspaceBox/Coding4Fun.WorkspaceBoxPackage.cs
@@ -12,179 +12,203 @@ using Microsoft.VisualStudio.TeamFoundation.VersionControl;
 
 namespace Aquila.Coding4Fun_WorkspaceBox
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(GuidList.GuidCoding4Fun_WorkspaceBoxPkgString)]
-    [ProvideAutoLoad("F1536EF8-92EC-443C-9ED7-FDADF150DA82")]
-    public sealed class Coding4Fun_WorkspaceBoxPackage : Package
-    {
-        private readonly int _workspaceBoxId;
-        private readonly int _checkoutId;
+	[PackageRegistration(UseManagedResourcesOnly = true)]
+	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	[Guid(GuidList.GuidCoding4Fun_WorkspaceBoxPkgString)]
+	[ProvideAutoLoad("F1536EF8-92EC-443C-9ED7-FDADF150DA82")]
+	public sealed class Coding4Fun_WorkspaceBoxPackage : Package
+	{
+		private readonly int _workspaceBoxId;
+		private readonly int _checkoutId;
 
-        private string _currentDirectory;
-        private string _currentFile;
-        private WorkspaceInfo _workspaceInfo;
+		private string _currentDirectory;
+		private string _currentFile;
+		private WorkspaceInfo _workspaceInfo;
 
-        public Coding4Fun_WorkspaceBoxPackage()
-        {
-            _workspaceBoxId = (int)PkgCmdIDList.cmdidWorkspaceBoxCmd;
-            _checkoutId = (int)PkgCmdIDList.cmdidCheckoutCmd;
-        }
+		public Coding4Fun_WorkspaceBoxPackage()
+		{
+			_workspaceBoxId = (int)PkgCmdIDList.cmdidWorkspaceBoxCmd;
+			_checkoutId = (int)PkgCmdIDList.cmdidCheckoutCmd;
+		}
 
-        #region Package Members
-        protected override void Initialize()
-        {
-            base.Initialize();
+		#region Package Members
+		protected override void Initialize()
+		{
+			base.Initialize();
 
-            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if (null == mcs)
-            {
-                return;
-            }
+			var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+			if (null == mcs)
+			{
+				return;
+			}
 
-            var cmdId = new CommandID(GuidList.GuidCoding4FunCmdSet, _workspaceBoxId);
-            var workspaceItem = new OleMenuCommand(OnGoToSourceControl, cmdId);
-            workspaceItem.BeforeQueryStatus += OnExec;
-            mcs.AddCommand(workspaceItem);
+			var cmdId = new CommandID(GuidList.GuidCoding4FunCmdSet, _workspaceBoxId);
+			var workspaceItem = new OleMenuCommand(OnGoToSourceControl, cmdId);
+			workspaceItem.BeforeQueryStatus += OnExec;
+			mcs.AddCommand(workspaceItem);
 
-            var checkoutBtnId = new CommandID(GuidList.GuidCoding4FunCmdSet, _checkoutId);
-            var checkoutItem = new OleMenuCommand(OnCheckoutExec, checkoutBtnId);
-            checkoutItem.BeforeQueryStatus += OnCheckoutQueryStatus;
-            mcs.AddCommand(checkoutItem);
-        }
-        #endregion
+			var checkoutBtnId = new CommandID(GuidList.GuidCoding4FunCmdSet, _checkoutId);
+			var checkoutItem = new OleMenuCommand(OnCheckoutExec, checkoutBtnId);
+			checkoutItem.BeforeQueryStatus += OnCheckoutQueryStatus;
+			mcs.AddCommand(checkoutItem);
+		}
+		#endregion
 
-        private void OnExec(object sender, EventArgs e)
-        {
-            var menuCommand = sender as OleMenuCommand;
-            if (menuCommand != null)
-            {
-                menuCommand.Text = GetCurrentWorkspace();
-            }
-        }
+		private void OnExec(object sender, EventArgs e)
+		{
+			var menuCommand = sender as OleMenuCommand;
+			if (menuCommand != null)
+			{
+				menuCommand.Text = GetCurrentWorkspace();
+			}
+		}
 
-        private void OnGoToSourceControl(object sender, EventArgs e)
-        {
-            var menuCommand = sender as OleMenuCommand;
-            if (menuCommand != null)
-            {
-                var app = (DTE)GetService(typeof(SDTE));
+		private void OnGoToSourceControl(object sender, EventArgs e)
+		{
+			var menuCommand = sender as OleMenuCommand;
+			if (menuCommand != null)
+			{
+				var app = (DTE)GetService(typeof(SDTE));
 
-                if (app.ActiveDocument != null)
-                {
-                    var path = app.ActiveDocument.FullName;
-                    var vc = app.GetObject("Microsoft.VisualStudio.TeamFoundation.VersionControl.VersionControlExt") as VersionControlExt;
-                    if (vc != null)
-                    {
-                        _workspaceInfo = Workstation.Current.GetLocalWorkspaceInfo(path);
-                        var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
-                        var workspace = _workspaceInfo.GetWorkspace(server);
-                        var serverPath = workspace.GetServerItemForLocalItem(path);
-                        vc.Explorer.Navigate(serverPath);
-                        app.ExecuteCommand("View.TfsSourceControlExplorer");
-                    }
-                }
-            }
-        }
+				if (app.ActiveDocument != null)
+				{
+					var path = app.ActiveDocument.FullName;
+					var vc = app.GetObject("Microsoft.VisualStudio.TeamFoundation.VersionControl.VersionControlExt") as VersionControlExt;
+					if (vc != null &&
+						vc.Explorer != null &&
+						vc.Explorer.Connected)
+					{
+						_workspaceInfo = Workstation.Current.GetLocalWorkspaceInfo(path);
+						var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
+						var workspace = _workspaceInfo.GetWorkspace(server);
+						var serverPath = workspace.GetServerItemForLocalItem(path);
+						vc.Explorer.Navigate(serverPath);
+						app.ExecuteCommand("View.TfsSourceControlExplorer");
+					}
+				}
+			}
+		}
 
-        private void OnCheckoutQueryStatus(object sender, EventArgs e)
-        {
-            var menuCommand = sender as OleMenuCommand;
-            if (menuCommand == null)
-            {
-                return;
-            }
+		private void OnCheckoutQueryStatus(object sender, EventArgs e)
+		{
+			var menuCommand = sender as OleMenuCommand;
+			if (menuCommand == null)
+			{
+				return;
+			}
 
-            var dte = (_DTE)GetService(typeof(_DTE));
-            var doc = dte.ActiveDocument == null
-                ? null
-                : (TextDocument)dte.ActiveDocument.Object();
-            if (doc != null && _currentFile == dte.ActiveDocument.FullName)
-            {
-                return;
-            }
-            _currentFile = dte.ActiveDocument.FullName;
-            menuCommand.Supported = false;
-            if (!HasWorkspace() || HasPandingChangesAlready())
-            {
-                return;
-            }
+			var dte = (_DTE)GetService(typeof(_DTE));
+			var doc = dte.ActiveDocument == null
+				? null
+				: (TextDocument)dte.ActiveDocument.Object();
+			if (doc == null ||
+				dte.ActiveWindow != ((EnvDTE.Document)(dte.ActiveDocument)).ActiveWindow)
+			{
+				_currentFile = string.Empty;
+				menuCommand.Supported = false;
+				return;
+			}
+			if (_currentFile == dte.ActiveDocument.FullName)
+			{
+				return;
+			}
 
-            menuCommand.Supported = true;
-            menuCommand.Visible = true;
-            menuCommand.Text = Resources.CheckoutCurrentFile;
-        }
+			menuCommand.Supported = false;
+			_currentFile = dte.ActiveDocument.FullName;
+			if (!HasWorkspace() || HasPandingChangesAlready())
+			{
+				return;
+			}
 
-        private void OnCheckoutExec(object sender, EventArgs e)
-        {
-            var menuCommand = sender as OleMenuCommand;
-            if (menuCommand != null)
-            {
-                CheckoutCurFile();
-            }
-        }
+			menuCommand.Supported = true;
+			menuCommand.Text = Resources.CheckoutCurrentFile;
+		}
 
-        private bool HasWorkspace()
-        {
-            return _workspaceInfo != null;
-        }
+		private void OnCheckoutExec(object sender, EventArgs e)
+		{
+			var menuCommand = sender as OleMenuCommand;
+			if (menuCommand != null)
+			{
+				CheckoutCurFile();
+			}
+		}
 
-        private string GetCurrentWorkspace()
-        {
-            var dte = (_DTE)GetService(typeof(_DTE));
-            if (string.IsNullOrEmpty(_currentDirectory) ||
-                _currentDirectory != dte.Solution.FullName)
-            {
-                _currentDirectory = dte.Solution.FullName;
-                _workspaceInfo = Workstation.Current.GetLocalWorkspaceInfo(_currentDirectory);
-            }
-            return HasWorkspace() ? _workspaceInfo.DisplayName : Resources.NoWorkspace;
-        }
+		private bool HasWorkspace()
+		{
+			return _workspaceInfo != null;
+		}
 
-        private bool HasPandingChangesAlready()
-        {
-            var app = (DTE)GetService(typeof(SDTE));
-            var doc = app.ActiveDocument == null
-                ? null
-                : (TextDocument)app.ActiveDocument.Object();
-            if (!IsCanPendEdit(doc) || app.ActiveDocument == null)
-            {
-                return false;
-            }
+		private string GetCurrentWorkspace()
+		{
+			var dte = (_DTE)GetService(typeof(_DTE));
+			if (string.IsNullOrEmpty(_currentDirectory) ||
+				_currentDirectory != dte.Solution.FullName)
+			{
+				if (!string.IsNullOrEmpty(dte.Solution.FullName))
+				{
+					_currentDirectory = dte.Solution.FullName;
+					_workspaceInfo = Workstation.Current.GetLocalWorkspaceInfo(_currentDirectory);
+				}
+				else
+				{
+					_workspaceInfo = null;
+					_currentDirectory = string.Empty;
+				}
+			}
+			return HasWorkspace() ? _workspaceInfo.DisplayName : Resources.NoWorkspace;
+		}
 
-            var activeDocumentFullName = app.ActiveDocument.FullName;
-            var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
-            var workspace = _workspaceInfo.GetWorkspace(server);
-            return workspace.GetPendingChanges(activeDocumentFullName).Any();
-        }
+		private bool HasPandingChangesAlready()
+		{
+			var app = (DTE)GetService(typeof(SDTE));
+			var doc = app.ActiveDocument == null
+				? null
+				: (TextDocument)app.ActiveDocument.Object();
+			if (!IsCanPendEdit(doc) || app.ActiveDocument == null)
+			{
+				return false;
+			}
 
-        private void CheckoutCurFile()
-        {
-            var app = (DTE)GetService(typeof(SDTE));
-            var doc = app.ActiveDocument == null
-                ? null
-                : (TextDocument)app.ActiveDocument.Object();
-            if (HasWorkspace() && IsCanPendEdit(doc) && app.ActiveDocument != null)
-            {
-                var activeDocumentFullName = app.ActiveDocument.FullName;
-                var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
-                var workspace = _workspaceInfo.GetWorkspace(server);
-                workspace.PendEdit(activeDocumentFullName);
+			var activeDocumentFullName = app.ActiveDocument.FullName;
+			var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
+			var workspace = _workspaceInfo.GetWorkspace(server);
+			return workspace.GetPendingChanges(activeDocumentFullName).Any();
+		}
 
-                RefreshPendingChanges(app);
-            }
-        }
+		private void CheckoutCurFile()
+		{
+			var app = (DTE)GetService(typeof(SDTE));
+			var doc = app.ActiveDocument == null
+				? null
+				: (TextDocument)app.ActiveDocument.Object();
+			if (HasWorkspace() && IsCanPendEdit(doc) && app.ActiveDocument != null)
+			{
+				var activeDocumentFullName = app.ActiveDocument.FullName;
+				var server = new TfsTeamProjectCollection(_workspaceInfo.ServerUri);
+				var workspace = _workspaceInfo.GetWorkspace(server);
+				workspace.PendEdit(activeDocumentFullName);
 
-        private static bool IsCanPendEdit(TextDocument doc)
-        {
-            return doc != null && doc.Type.Equals("Text");
-        }
+				RefreshPendingChanges(app);
+			}
+		}
 
-        private static void RefreshPendingChanges(_DTE application)
-        {
-            application.ExecuteCommand("View.TfsPendingChanges");
-            application.ExecuteCommand("View.Refresh");
-        }
-    }
+		private static bool IsCanPendEdit(TextDocument doc)
+		{
+			return doc != null && doc.Type.Equals("Text");
+		}
+
+		private static void RefreshPendingChanges(_DTE application)
+		{
+			try
+			{
+				application.ExecuteCommand("View.TfsPendingChanges");
+				application.ExecuteCommand("View.Refresh");
+			}
+			catch (COMException)
+			{
+				//TODO: add notification
+			}
+		}
+	}
 }
--- Coding4Fun.WorkspaceBox/Guids.cs
+++ Coding4Fun.WorkspaceBox/Guids.cs
@@ -4,11 +4,11 @@ using System;
 
 namespace Aquila.Coding4Fun_WorkspaceBox
 {
-    static class GuidList
-    {
-        public const string GuidCoding4Fun_WorkspaceBoxPkgString = "EC96722B-F4FB-432B-B4C3-5A3B33539698";
-        public const string GuidCoding4FunCmdSetString = "1508C2F6-4C2C-4880-B5E2-64750036AADF";
+	static class GuidList
+	{
+		public const string GuidCoding4Fun_WorkspaceBoxPkgString = "EC96722B-F4FB-432B-B4C3-5A3B33539698";
+		public const string GuidCoding4FunCmdSetString = "1508C2F6-4C2C-4880-B5E2-64750036AADF";
 
-        public static readonly Guid GuidCoding4FunCmdSet = new Guid(GuidCoding4FunCmdSetString);
-    };
+		public static readonly Guid GuidCoding4FunCmdSet = new Guid(GuidCoding4FunCmdSetString);
+	};
 }
